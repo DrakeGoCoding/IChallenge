98b30ef2fe11b2de3c527f552c719ec006739ed3
phuonghaa <phuonggha39@gmail.com>
2020-12-24 15:41:18 +0700

    homepage design

---
+++ components/createPlusBtn.js
@@ -0,0 +1,43 @@
+const style = `
+.create-btn{
+    position: fixed;
+    color: #fff;
+    font-size: 60px;
+    left: 90vw;
+    top: 80vh;
+}
+.create-btn:hover{
+    cursor: pointer;
+    text-shadow: 4px 4px 0 #69C9D0,-4px -4px 0 #EE1D52;
+}
+@media only screen and (max-width: 768px){
+    .create-btn{
+        font-size: 50px;
+        left: 92vw;
+        top:85vh;
+    }
+}
+@media only screen and (max-width: 400px){
+    .create-btn{
+        left: 88vw;
+    }
+}
+`
+class CreatePlusBtn extends HTMLElement{
+    constructor(){
+        super()
+        this._shadowDom = this.attachShadow({mode: 'open'})
+    }
+
+    connectedCallback(){
+        this._shadowDom.innerHTML=`
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+        ${style}
+        </style>
+        <div class="create-btn"> <i class="fa fa-plus-circle"></i> </div>
+        `
+    }
+}
+
+window.customElements.define('create-plus-btn', CreatePlusBtn)
---
+++ index.js
@@ -0,0 +1,12 @@
+import './screens/home.js'
+
+import'./components/homeHeader.js'
+import'./components/createPlusBtn.js'
+import'./components/quizInfoList.js'
+import'./components/quizInfoItem.js'
+
+
+
+document.getElementById('container').innerHTML=`
+    <home-screen></home-screen>
+`
