d5d4829e8371c4b2e178247268b05c712f230a9c
phuonghaa <phuonggha39@gmail.com>
2020-12-25 17:04:30 +0700

    add QuizRecord design

--- index.js
+++ index.js
@@ -11,20 +11,29 @@ import './components/quizCreator/previewItem.js'
 import './components/quizCreator/addQuestionBtn.js'
 import './components/quizCreator/quizInput.js'
 
-import'./screens/quizDisplay.js'
+import './screens/quizDisplay.js'
 import './components/quizDisplay/quizHeader.js'
 import './components/quizDisplay/quizContainer.js'
 
+import './screens/quizRecords.js'
+import './components/quizRecord/recordContainer.js'
+
 //enable/ disable the comments for displaying the screens
+
+
 
 // document.getElementById('container').innerHTML=`
 //     <home-screen></home-screen>
 // `
 
+document.getElementById('container').innerHTML=`
+    <quiz-creator></quiz-creator>
+`
+
 // document.getElementById('container').innerHTML=`
-//     <quiz-creator></quiz-creator>
+//     <quiz-display></quiz-display>
 // `
 
-document.getElementById('container').innerHTML=`
-    <quiz-display></quiz-display>
-`
+// document.getElementById('container').innerHTML=`
+//     <quiz-record></quiz-record>
+// `
