692ad0a09743ddbfcf9843091490f5f94e7ee5e5
DrakeGoCoding <hoanglongphi21@gmail.com>
2021-01-05 20:05:43 +0700

    nah

--- navigo.js
+++ navigo.js
@@ -1,13 +1,15 @@
-import { getAccountDocByUserName } from './utils.js'
+import { getItemFromLocalStorage } from './utils.js'
 
 var root = null;
 var useHash = true; // Defaults to: false
 var hash = '#!'; // Defaults to: '#'
 var router = new Navigo(root, useHash, hash);
-import { getItemFromLocalStorage } from './utils.js'
 
 router
     .on({
+        'homepage-screen': function() {
+            redirect('homepage-screen')
+        },
         'login-screen': function() {
             redirect('login-screen')
         },
@@ -26,10 +28,6 @@ router
         '*': function() {
             router.navigate('home-screen')
         },
-        'homepage-screen': function() {
-            redirect('homepage-screen')
-        }
-
 
     })
     .resolve();
@@ -37,41 +35,41 @@ router
 function redirect(screenName) {
     if (screenName === 'home-screen') {
         document.getElementById('container').innerHTML = `
-        <home-screen></home-screen>
+            <home-screen></home-screen>
         `
     } else if (screenName === 'signup-screen') {
         document.getElementById('container').innerHTML = `
-        <signUp-screen></signUp-screen>
+            <signUp-screen></signUp-screen>
         `
     } else if (screenName === 'login-screen') {
         document.getElementById('container').innerHTML = `
-        <login-screen></login-screen>
+            <login-screen></login-screen>
         `
     } else if (screenName === 'quiz-creator') {
         document.getElementById('container').innerHTML = `
-        <quiz-creator></quiz-creator>
+            <quiz-creator></quiz-creator>
         `
     } else if (screenName == 'quiz-starter') {
         document.getElementById('container').innerHTML = `
-        <quiz-starter></quiz-starter>
+            <quiz-starter></quiz-starter>
         `
     } else if (screenName == 'quiz-display') {
         document.getElementById('container').innerHTML = `
-        <quiz-display></quiz-display>
+            <quiz-display></quiz-display>
         `
     } else if (screenName == 'quiz-record') {
         document.getElementById('container').innerHTML = `
-        <quiz-record></quiz-record>
+            <quiz-record></quiz-record>
         `
     } else if (screenName == 'homepage-screen') {
         document.getElementById('container').innerHTML = `
-        <homepage-screen></homepage-screen>
+            <homepage-screen></homepage-screen>
         `
     }
 }
+
 async function checkAuthen() {
     const user = getItemFromLocalStorage('currentUser');
-
     if (user) {
         const res = await firebase.firestore()
             .collection('Accounts')
