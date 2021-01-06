6ecc66877fc7f186e6e67c3f8a4d5c241f1f56fa
DrakeGoCoding <hoanglongphi21@gmail.com>
2021-01-07 00:21:18 +0700

    nah

--- component/quizCreator/creatorHeader.js
+++ component/quizCreator/creatorHeader.js
@@ -37,7 +37,7 @@ const style = `
         position: fixed;
         top: 0;
         width: 100%;
-        height: 80px;
+        height: 100px;
         background-color: #010101;
         display: flex;
         justify-content: space-between;
@@ -67,7 +67,7 @@ const style = `
         cursor: pointer;
         text-shadow: 4px 4px 0 #69C9D0,-4px -4px 0 #EE1D52;
     }
-    @media only screen and (max-width: 1000px){
+    @media only screen and (max-width: 786px){
         .main-header{
             height: 70px;
         }
@@ -78,7 +78,7 @@ const style = `
             font-size: 35px;
         }
     }
-    @media only screen and (max-width: 400px){
+    @media only screen and (max-width: 425px){
         .main-header{
             height: 80px
         }
