e184425075833509cb1c51e46ca9f20e6e45701c
Lê Quang Minh <lequangminh100802@gmail.com>
2021-01-07 00:22:13 +0700

    Update homePage.js

--- screen/homePage.js
+++ screen/homePage.js
@@ -109,7 +109,7 @@ transition: .6s;
         position: fixed;
         width: 100%;
         height: 100vh;
-        background: var(--clr-darker-background);
+        background: #0d0d0e;
         top: 80px;
         left: -200%;
         text-align: center;
@@ -401,6 +401,9 @@ height: 190%;
         padding: 10px 20px;
         font-size: 20px;
     }
+    #check:checked ~ ul{
+        left: -7%;
+    } 
 }
 @media (max-width: 620px) {
     header {
@@ -434,6 +437,9 @@ height: 190%;
         padding: 10px 10px;
         font-size: 20px;
     }
+    #check:checked ~ ul{
+        left: -8%;
+    } 
 }
 @media (max-width: 480px) {
     header {
@@ -470,6 +476,9 @@ height: 190%;
         padding: 10px 50px;
         font-size: 20px;
     }
+    #check:checked ~ ul{
+        left: -11%;
+    } 
 }
 @media (max-width: 400px) {
     header {
@@ -507,6 +516,9 @@ height: 190%;
         font-size: 15px;
         transition: .5s;
     }
+    #check:checked ~ ul{
+        left: -12%;
+    } 
 }
 /* Footer */
 footer {
@@ -518,7 +530,7 @@ font-family: 'JetBrains Mono', monospace;
 box-sizing: border-box;
 bottom: 0px;
 width: 100%;
-background: var(--clr-darker-background);
+background: #0d0d0e;
 }
 .main-content {
 display: flex;
@@ -807,7 +819,7 @@ class HomePage extends HTMLElement {
             </main>
         
             <footer>
-                <div class="main-content" id="section@" >
+                <div class="main-content" id="section@">
                     <div class="left box">
                         <h2>What is Queazy?</h2>
                         <div class="content">
